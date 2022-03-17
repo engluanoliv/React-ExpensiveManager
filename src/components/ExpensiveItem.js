9c31525c141494b56855739223c68698a4e26466
engluanoliv <engluanoliv@gmail.com>
2022-03-17 00:41:34 -0300

    initial commit

---
+++ src/components/ExpensiveItem.js
@@ -0,0 +1,11 @@
+function ExpensiveItem () {
+    return(
+        <>
+            <div>Data</div>
+            <div><h2>Title</h2></div>
+            <div>Amount</div>
+        </>
+    );
+}
+
+export default ExpensiveItem;
